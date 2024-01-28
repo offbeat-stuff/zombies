0af81136f7824021cadc2aaa8da3cda2dc8a223d
Artix Kai <artix@github.com>
2024-01-28 18:32:48 +0530

    shields also follow scaling now

--- src/main/java/org/codeberg/zenxarch/zombies/difficulty/ExtendedDifficulty.java
+++ src/main/java/org/codeberg/zenxarch/zombies/difficulty/ExtendedDifficulty.java
@@ -168,7 +168,7 @@ public class ExtendedDifficulty {
 
     @Override
     public Optional<Item> getEquipmentForSlot(EquipmentSlot slot) {
-      if (slot.equals(EquipmentSlot.OFFHAND) && random.nextDouble() < 0.05) {
+      if (slot.equals(EquipmentSlot.OFFHAND) && random.nextDouble() < (0.05 * this.progress)) {
         return Optional.of(Items.SHIELD);
       }
 
