a6d57388f8f695b3c2700a677b8e2a3bb569dd10
ZenXArch@fedora <ZenXArch@fedora.mail>
2024-02-12 21:15:17 +0530

    seperated spawn logic + world height consious

---
+++ src/main/java/org/codeberg/zenxarch/zombies/spawning/SpawnProvider.java
@@ -0,0 +1,76 @@
+package org.codeberg.zenxarch.zombies.spawning;
+
+import java.util.Optional;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.SpawnRestriction;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.registry.tag.BiomeTags;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.LightType;
+import net.minecraft.world.SpawnHelper;
+import org.codeberg.zenxarch.zombies.debug.Debug;
+
+public class SpawnProvider {
+
+  private final Random random = Random.create();
+  private ServerWorld world;
+  private final Debug debug;
+
+  public SpawnProvider(ServerWorld world, Debug debug) {
+    this.world = world;
+    this.debug = debug;
+  }
+
+  private boolean canSpawnAtPosBasic(BlockPos pos) {
+    if (this.world.getLightLevel(LightType.BLOCK, pos) > 0 ||
+        this.world.isPlayerInRange(pos.getX(), pos.getY(), pos.getZ(), 16) ||
+        this.world.getBiome(pos).isIn(BiomeTags.WITHOUT_ZOMBIE_SIEGES) ||
+        !SpawnHelper.canSpawn(SpawnRestriction.getLocation(EntityType.ZOMBIE),
+                              this.world, pos, EntityType.ZOMBIE) ||
+        !MobEntity.canMobSpawn(EntityType.ZOMBIE, this.world,
+                               SpawnReason.NATURAL, pos, this.world.random)) {
+      return false;
+    };
+
+    this.debug.spawnCheck();
+
+    var boundingBox = EntityType.ZOMBIE.createSimpleBoundingBox(
+        pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5);
+
+    return world.doesNotIntersectEntities(null,
+                                          VoxelShapes.cuboid(boundingBox)) &&
+        world.isSpaceEmpty(boundingBox) && !world.containsFluid(boundingBox);
+  }
+
+  private static int SPAWN_RANGE = 64;
+
+  private BlockPos giveRandomPos(BlockPos center) {
+    int x = this.random.nextBetween(-SPAWN_RANGE, SPAWN_RANGE);
+    int z = this.random.nextBetween(-SPAWN_RANGE, SPAWN_RANGE);
+
+    int minY = Math.max(this.world.getBottomY(), center.getY() - SPAWN_RANGE);
+
+    int maxY = Math.min(
+        this.world.getTopY(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, x, z),
+        center.getY() + SPAWN_RANGE);
+
+    return center.add(x, this.random.nextBetween(minY, maxY), z);
+  }
+
+  public Optional<BlockPos> giveSpawnPos(ServerWorld world, BlockPos centerPos,
+                                         int times) {
+    this.world = world;
+    for (int i = 0; i < times; i++) {
+      var pos = giveRandomPos(centerPos);
+      if (canSpawnAtPosBasic(pos)) {
+        return Optional.of(pos);
+      }
+    }
+    return Optional.empty();
+  }
+}
--- src/main/java/org/codeberg/zenxarch/zombies/spawning/ZombieApocalypse.java
+++ src/main/java/org/codeberg/zenxarch/zombies/spawning/ZombieApocalypse.java
@@ -2,22 +2,13 @@ package org.codeberg.zenxarch.zombies.spawning;
 
 import java.util.Optional;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnReason;
-import net.minecraft.entity.SpawnRestriction;
-import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.registry.tag.BiomeTags;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.random.Random;
-import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.GameRules;
-import net.minecraft.world.LightType;
-import net.minecraft.world.SpawnHelper;
 import net.minecraft.world.World;
 import net.minecraft.world.spawner.SpecialSpawner;
 import org.codeberg.zenxarch.zombies.Zombies;
@@ -28,43 +19,13 @@ import org.codeberg.zenxarch.zombies.difficulty.ExtendedZombieEntity;
 
 public class ZombieApocalypse implements SpecialSpawner {
 
-  private final Random random = Random.create();
   private ServerWorld world;
-  private Debug debug = new Debug();
+  private final Debug debug = new Debug();
+  private final SpawnProvider spawnProvider;
 
-  public ZombieApocalypse(ServerWorld world) { this.world = world; }
-
-  private boolean canSpawnAtPosBasic(BlockPos pos) {
-    if (world.getLightLevel(LightType.BLOCK, pos) > 0) {
-      return false;
-    }
-
-    if (this.world.isPlayerInRange(pos.getX(), pos.getY(), pos.getZ(), 16)) {
-      return false;
-    }
-
-    if (this.world.getBiome(pos).isIn(BiomeTags.WITHOUT_ZOMBIE_SIEGES)) {
-      return false;
-    }
-
-    var entityType = EntityType.ZOMBIE;
-    var location = SpawnRestriction.getLocation(entityType);
-
-    if (!SpawnHelper.canSpawn(location, this.world, pos, entityType) ||
-        !MobEntity.canMobSpawn(EntityType.ZOMBIE, this.world,
-                               SpawnReason.NATURAL, pos, this.world.random)) {
-      return false;
-    };
-
-    debug.spawnCheck();
-
-    var boundingBox = EntityType.ZOMBIE.createSimpleBoundingBox(
-        pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5);
-
-    return this.world.doesNotIntersectEntities(
-               null, VoxelShapes.cuboid(boundingBox)) &&
-        this.world.isSpaceEmpty(boundingBox) &&
-        !this.world.containsFluid(boundingBox);
+  public ZombieApocalypse(ServerWorld world) {
+    this.world = world;
+    this.spawnProvider = new SpawnProvider(world, debug);
   }
 
   private boolean canSpawnAtPosSpace(ZombieEntity zombie) {
@@ -73,25 +34,12 @@ public class ZombieApocalypse implements SpecialSpawner {
         !this.world.containsFluid(zombie.getBoundingBox());
   }
 
-  private static int SPAWN_RANGE = 64;
-
-  private Optional<BlockPos> findNearestWorking(BlockPos pos, int times) {
-    for (int i = 0; i < times; i++) {
-      var p = pos.add(random.nextBetween(-SPAWN_RANGE, SPAWN_RANGE),
-                      random.nextBetween(-SPAWN_RANGE, SPAWN_RANGE),
-                      random.nextBetween(-SPAWN_RANGE, SPAWN_RANGE));
-      if (canSpawnAtPosBasic(p)) {
-        return Optional.of(p);
-      }
-    }
-    return Optional.empty();
-  }
-
   public boolean spawnZombieAt(BlockPos ppos) {
     var difficulty = new ExtendedDifficultyInfo(this.world, ppos);
     var zombieOpt =
-        findNearestWorking(ppos,
-                           ExtendedDifficulty.getTriesForSpawning(difficulty))
+        spawnProvider
+            .giveSpawnPos(world, ppos,
+                          ExtendedDifficulty.getTriesForSpawning(difficulty))
             .map(u -> new ExtendedZombieEntity(this.world, u))
             .filter(this::canSpawnAtPosSpace);
 
@@ -111,11 +59,8 @@ public class ZombieApocalypse implements SpecialSpawner {
   public int spawn(ServerWorld world, boolean spawnMonsters,
                    boolean spawnAnimals) {
     this.world = world;
-    if (!spawnMonsters) {
-      return 0;
-    }
-
-    if (this.world.getDifficulty().equals(Difficulty.PEACEFUL) ||
+    if (!spawnMonsters ||
+        this.world.getDifficulty().equals(Difficulty.PEACEFUL) ||
         !this.world.getGameRules().getBoolean(GameRules.DO_MOB_SPAWNING)) {
       return 0;
     }
