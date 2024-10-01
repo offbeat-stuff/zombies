828b49bb83faba7fc3605528aa10c580411fe14c
ZenXArch@fedora <ZenXArch@fedora.mail>
2024-10-01 22:47:29 +0530

    add google-java-format

--- tidy-all.sh
+++ tidy-all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd src/
 find -iname "*.java" | xargs clang-format -i
+find -iname "*.java" | xargs google-java-format --fix-imports-only -i
 cd ../nim
 find -iname "*.nim" | xargs nimpretty
