a50ef6a2dc71d620d4d1a1a65bd581b71f4d3ea4
ZenXArch@fedora <ZenXArch@fedora.mail>
2024-10-04 20:29:05 +0530

    Use reflective config for better code

--- src/main/java/org/codeberg/zenxarch/zombies/config/SpawnConfig.java
+++ src/main/java/org/codeberg/zenxarch/zombies/config/SpawnConfig.java
@@ -1,15 +1,19 @@
 package org.codeberg.zenxarch.zombies.config;
 
-import folk.sisby.kaleido.api.WrappedConfig;
+import folk.sisby.kaleido.api.ReflectiveConfig;
 import folk.sisby.kaleido.lib.quiltconfig.api.annotations.Comment;
 import folk.sisby.kaleido.lib.quiltconfig.api.annotations.IntegerRange;
+import folk.sisby.kaleido.lib.quiltconfig.api.annotations.SerializedName;
+import folk.sisby.kaleido.lib.quiltconfig.api.values.TrackedValue;
 
-public class SpawnConfig extends WrappedConfig {
+public class SpawnConfig extends ReflectiveConfig {
   @Comment("Distance near player along which no spawns shall occur")
+  @SerializedName("no_spawn_near_player_range")
   @IntegerRange(min = 0, max = 128)
-  public int NO_SPAWN_NEAR_PLAYER_DISTANCE = 16;
+  public TrackedValue<Integer> NO_SPAWN_NEAR_PLAYER_RANGE = this.value(16);
 
   @Comment("Range from player within which random spawn positions will be used")
+  @SerializedName("spawn_range_from_initial_point")
   @IntegerRange(min = 0, max = 128)
-  public int SPAWN_RANGE_FROM_INITIAL_POINT = 64;
+  public TrackedValue<Integer> SPAWN_RANGE_FROM_INITIAL_POINT = this.value(64);
 }
