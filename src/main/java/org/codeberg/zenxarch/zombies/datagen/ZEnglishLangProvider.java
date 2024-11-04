247bdb56e5f41987b4f14ae3e466fb52702aac27
ZenXArch@fedora <ZenXArch@fedora.mail>
2024-11-04 14:53:19 +0530

    Add descriptions

--- src/main/java/org/codeberg/zenxarch/zombies/datagen/ZEnglishLangProvider.java
+++ src/main/java/org/codeberg/zenxarch/zombies/datagen/ZEnglishLangProvider.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.RegistryWrapper.WrapperLookup;
+import net.minecraft.world.GameRules;
 import org.codeberg.zenxarch.zombies.Zombies;
 
 public class ZEnglishLangProvider extends FabricLanguageProvider {
@@ -18,11 +19,31 @@ public class ZEnglishLangProvider extends FabricLanguageProvider {
   @Override
   public void generateTranslations(
       WrapperLookup registryLookup, TranslationBuilder translationBuilder) {
-    translationBuilder.add(
-        Zombies.BASE_ZOMBIES.getTranslationKey(), "Minimum numbers of zombies to spawn");
-    translationBuilder.add(
-        Zombies.MAX_ZOMBIES.getTranslationKey(),
-        "Maximum (* difficulty) numbers of zombies to spawn");
-    translationBuilder.add(Zombies.SPAWN_SPEED.getTranslationKey(), "Fill zombie cap over seconds");
+    addGameruleTranslation(
+        translationBuilder,
+        Zombies.BASE_ZOMBIES,
+        "Minimum number of zombies to spawn",
+        "Zombies mod tries to spawn at least this many zombies (around a player)");
+    addGameruleTranslation(
+        translationBuilder,
+        Zombies.MAX_ZOMBIES,
+        "Maximum (* difficulty) numbers of zombies to spawn",
+        "This number * globalDifficulty (easy -> 1.0,normal -> 2.0,hard -> 3.0) is the maximum"
+            + " amount of zombies to spawned around a player");
+    addGameruleTranslation(
+        translationBuilder,
+        Zombies.SPAWN_SPEED,
+        "Fill zombie cap over seconds",
+        "The mod tries to spawn the target amount of zombies (difficulty dependent) over this many"
+            + " seconds assuming 1% of positions are spawnable");
+  }
+
+  public static void addGameruleTranslation(
+      TranslationBuilder translationBuilder,
+      GameRules.Key<?> gamerule,
+      String name,
+      String description) {
+    translationBuilder.add(gamerule.getTranslationKey(), name);
+    translationBuilder.add(gamerule.getTranslationKey() + ".description", description);
   }
 }
