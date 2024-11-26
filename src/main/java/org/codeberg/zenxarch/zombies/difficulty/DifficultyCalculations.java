0d20c41423322d8aab14cdcf55a74f47e71e94e7
ZenXArch@fedora <ZenXArch@fedora.mail>
2024-11-26 14:39:31 +0530

    make it so moon size is not needed for maximum difficulty

--- src/main/java/org/codeberg/zenxarch/zombies/difficulty/DifficultyCalculations.java
+++ src/main/java/org/codeberg/zenxarch/zombies/difficulty/DifficultyCalculations.java
@@ -39,7 +39,8 @@ public abstract class DifficultyCalculations {
 
     var playerScore = players == 0 ? 0.0 : scoreSum / players;
     playerScore = MathHelper.clamp(playerScore, 0.0, 1.0);
-    var timeFactor = (dayFactor * inhabitedTimeFactor) * 0.5 * (1.0 + moonSize);
+    var timeFactor =
+        MathHelper.clamp((dayFactor * inhabitedTimeFactor) * (1.0 + moonSize), 0.0, 1.0);
 
     var killScore = players == 0 ? 0.0 : zombieKills / players;
     killScore = MathHelper.clamp(killScore / 2500, 0.0, 1.0);
