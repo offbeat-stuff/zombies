966e1cc2f4d18df4a53069b935a7ecbfb306e13b
ZenXArch@fedora <ZenXArch@fedora.mail>
2025-03-30 18:56:21 +0530

    tiny change

--- src/main/java/org/codeberg/zenxarch/zombies/entity/spawn_conditions/ZombieSpawnConditions.java
+++ src/main/java/org/codeberg/zenxarch/zombies/entity/spawn_conditions/ZombieSpawnConditions.java
@@ -1,5 +1,7 @@
 package org.codeberg.zenxarch.zombies.entity.spawn_conditions;
 
+import com.mojang.serialization.MapCodec;
+import net.minecraft.entity.spawn.SpawnCondition;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import org.codeberg.zenxarch.zombies.Zombies;
@@ -10,13 +12,13 @@ public final class ZombieSpawnConditions {
   }
 
   public static void initialize() {
-    Registry.register(
-        Registries.SPAWN_CONDITION_TYPE, Zombies.id("is_day"), DaySpawnCondition.CODEC);
-    Registry.register(
-        Registries.SPAWN_CONDITION_TYPE, Zombies.id("is_night"), NightSpawnCondition.CODEC);
-    Registry.register(
-        Registries.SPAWN_CONDITION_TYPE, Zombies.id("negate"), NegateSpawnCondition.CODEC);
-    Registry.register(
-        Registries.SPAWN_CONDITION_TYPE, Zombies.id("all_of"), AllOfSpawnCondition.CODEC);
+    register("is_day", DaySpawnCondition.CODEC);
+    register("is_night", NightSpawnCondition.CODEC);
+    register("negate", NegateSpawnCondition.CODEC);
+    register("all_of", AllOfSpawnCondition.CODEC);
+  }
+
+  private static void register(String id, MapCodec<? extends SpawnCondition> codec) {
+    Registry.register(Registries.SPAWN_CONDITION_TYPE, Zombies.id(id), codec);
   }
 }
