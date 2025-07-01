4accf580e9ceee848d1bf2c7eb76c8b8e596136b
ZenXArch@fedora <ZenXArch@fedora.mail>
2025-07-01 15:12:20 +0530

    add new mob attachment

--- src/main/java/org/codeberg/zenxarch/zombies/data/entity/MobAttachments.java
+++ src/main/java/org/codeberg/zenxarch/zombies/data/entity/MobAttachments.java
@@ -1,20 +1,24 @@
 package org.codeberg.zenxarch.zombies.data.entity;
 
+import static org.codeberg.zenxarch.zombies.Zombies.id;
+
 import net.fabricmc.fabric.api.attachment.v1.AttachmentRegistry;
 import net.fabricmc.fabric.api.attachment.v1.AttachmentSyncPredicate;
 import net.fabricmc.fabric.api.attachment.v1.AttachmentType;
 import net.minecraft.entity.EquipmentTable;
+import net.minecraft.entity.damage.DamageType;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.loot.LootTable;
 import net.minecraft.loot.context.LootContextParameters;
 import net.minecraft.loot.context.LootContextTypes;
 import net.minecraft.loot.context.LootWorldContext;
+import net.minecraft.registry.RegistryCodecs;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntryList;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.AssetInfo;
 import net.minecraft.world.LocalDifficulty;
-import org.codeberg.zenxarch.zombies.Zombies;
 import org.codeberg.zenxarch.zombies.data.entity.effect.MobEffect;
 
 public final class MobAttachments {
@@ -24,18 +28,22 @@ public final class MobAttachments {
 
   public static final AttachmentType<AssetInfo> TEXTURE_OVERRIDE =
       AttachmentRegistry.create(
-          Zombies.id("texture_override"),
+          id("texture_override"),
           builder ->
               builder
                   .persistent(AssetInfo.CODEC)
                   .syncWith(AssetInfo.PACKET_CODEC, AttachmentSyncPredicate.all()));
 
   public static final AttachmentType<EquipmentTable> EQUIPMENT_TABLE =
-      AttachmentRegistry.createPersistent(Zombies.id("equipment_table"), EquipmentTable.CODEC);
+      AttachmentRegistry.createPersistent(id("equipment_table"), EquipmentTable.CODEC);
 
   public static final AttachmentType<RegistryKey<LootTable>> LOOT_TABLE =
       AttachmentRegistry.createPersistent(
-          Zombies.id("loot_table"), RegistryKey.createCodec(RegistryKeys.LOOT_TABLE));
+          id("loot_table"), RegistryKey.createCodec(RegistryKeys.LOOT_TABLE));
+
+  public static final AttachmentType<RegistryEntryList<DamageType>> INVULNERABLE_TO =
+      AttachmentRegistry.createPersistent(
+          id("invulnerable_to"), RegistryCodecs.entryList(RegistryKeys.DAMAGE_TYPE));
 
   public static final AttachmentType<MobEffect> ON_SPAWN = createZombieEvent("on_spawn");
   public static final AttachmentType<MobEffect> ON_TICK = createZombieEvent("on_tick");
@@ -46,7 +54,7 @@ public final class MobAttachments {
   public static final AttachmentType<MobEffect> ON_KILLED = createZombieEvent("on_killed");
 
   private static AttachmentType<MobEffect> createZombieEvent(String id) {
-    return AttachmentRegistry.createPersistent(Zombies.id(id), MobEffect.CODEC);
+    return AttachmentRegistry.createPersistent(id(id), MobEffect.CODEC);
   }
 
   public static void initEquipment(
