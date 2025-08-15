81454560dbbe8ce8702b6f3a9645a2753fcb19d7
ZenXArch@fedora <ZenXArch@fedora.mail>
2025-08-15 13:59:55 +0530

    remove dependency on sbl just in case

--- src/main/java/org/codeberg/zenxarch/zombies/entity/behaviour/LeapAtUnreachableTargetBehaviour.java
+++ src/main/java/org/codeberg/zenxarch/zombies/entity/behaviour/LeapAtUnreachableTargetBehaviour.java
@@ -1,38 +1,35 @@
 package org.codeberg.zenxarch.zombies.entity.behaviour;
 
-import com.mojang.datafixers.util.Pair;
-import java.util.List;
-import net.minecraft.entity.ai.brain.MemoryModuleState;
-import net.minecraft.entity.ai.brain.MemoryModuleType;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.tslat.smartbrainlib.api.core.behaviour.custom.attack.LeapAtTarget;
-import net.tslat.smartbrainlib.object.MemoryTest;
-import net.tslat.smartbrainlib.registry.SBLMemoryTypes;
-import net.tslat.smartbrainlib.util.BrainUtil;
 
-public class LeapAtUnreachableTargetBehaviour<E extends MobEntity> extends LeapAtTarget<E> {
+// import net.tslat.smartbrainlib.api.core.behaviour.custom.attack.LeapAtTarget;
+// import net.tslat.smartbrainlib.object.MemoryTest;
+// import net.tslat.smartbrainlib.registry.SBLMemoryTypes;
+// import net.tslat.smartbrainlib.util.BrainUtil;
 
-  private static final MemoryTest MEMORY_REQUIREMENTS =
-      MemoryTest.builder(4)
-          .hasMemories(
-              MemoryModuleType.ATTACK_TARGET,
-              MemoryModuleType.CANT_REACH_WALK_TARGET_SINCE,
-              SBLMemoryTypes.TARGET_UNREACHABLE.get())
-          .noMemory(MemoryModuleType.ATTACK_COOLING_DOWN);
+public class LeapAtUnreachableTargetBehaviour<E extends MobEntity> {
+  // extends LeapAtTarget<E> {
 
-  @Override
-  protected List<Pair<MemoryModuleType<?>, MemoryModuleState>> getMemoryRequirements() {
-    return MEMORY_REQUIREMENTS;
-  }
+  // private static final MemoryTest MEMORY_REQUIREMENTS =
+  //     MemoryTest.builder(4)
+  //         .hasMemories(
+  //             MemoryModuleType.ATTACK_TARGET,
+  //             MemoryModuleType.CANT_REACH_WALK_TARGET_SINCE,
+  //             SBLMemoryTypes.TARGET_UNREACHABLE.get())
+  //         .noMemory(MemoryModuleType.ATTACK_COOLING_DOWN);
 
-  public LeapAtUnreachableTargetBehaviour(int delayTicks) {
-    super(delayTicks);
-  }
+  // @Override
+  // protected List<Pair<MemoryModuleType<?>, MemoryModuleState>> getMemoryRequirements() {
+  //   return MEMORY_REQUIREMENTS;
+  // }
 
-  @Override
-  protected boolean checkExtraStartConditions(ServerWorld level, E entity) {
-    return super.checkExtraStartConditions(level, entity)
-        && BrainUtil.getMemory(entity, MemoryModuleType.CANT_REACH_WALK_TARGET_SINCE) > 100;
-  }
+  // public LeapAtUnreachableTargetBehaviour(int delayTicks) {
+  //   super(delayTicks);
+  // }
+
+  // @Override
+  // protected boolean checkExtraStartConditions(ServerWorld level, E entity) {
+  //   return super.checkExtraStartConditions(level, entity)
+  //       && BrainUtil.getMemory(entity, MemoryModuleType.CANT_REACH_WALK_TARGET_SINCE) > 100;
+  // }
 }
--- src/main/java/org/codeberg/zenxarch/zombies/entity/behaviour/RideMobsBehaviourImpl.java
+++ src/main/java/org/codeberg/zenxarch/zombies/entity/behaviour/RideMobsBehaviourImpl.java
@@ -4,22 +4,24 @@ import java.util.Optional;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.MathHelper;
-import net.tslat.smartbrainlib.util.EntityRetrievalUtil;
+
+// import net.tslat.smartbrainlib.util.EntityRetrievalUtil;
 
 public interface RideMobsBehaviourImpl {
 
   public static void rideFlyingMobs(LivingEntity self, boolean towering) {
     if (!towering) return;
-    var nearestFlyingEntityOpt = findFlyingEntity(self);
+    // var nearestFlyingEntityOpt = findFlyingEntity(self);
+    var nearestFlyingEntityOpt = Optional.<LivingEntity>empty();
     if (nearestFlyingEntityOpt.isEmpty()) return;
     var nearestFlyingEntity = nearestFlyingEntityOpt.get();
     if (self.squaredDistanceTo(nearestFlyingEntity) > MathHelper.square(3)) return;
     self.startRiding(nearestFlyingEntity);
   }
 
-  private static Optional<LivingEntity> findFlyingEntity(LivingEntity self) {
-    return EntityRetrievalUtil.findEntity(self, 10, e -> isRideable(e.getType()));
-  }
+  // private static Optional<LivingEntity> findFlyingEntity(LivingEntity self) {
+  //   return EntityRetrievalUtil.findEntity(self, 10, e -> isRideable(e.getType()));
+  // }
 
   private static boolean isRideable(EntityType<?> type) {
     return type.equals(EntityType.PARROT) || type.equals(EntityType.CHICKEN);
