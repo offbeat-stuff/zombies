ffd0053753c6e5f04063407b204c1b40df34bb86
ZenXArch@fedora <ZenXArch@fedora.mail>
2025-09-07 01:25:29 +0530

    fix: different doors possible at same time

--- src/client/java/org/codeberg/zenxarch/zombies/client/ZombiesClient.java
+++ src/client/java/org/codeberg/zenxarch/zombies/client/ZombiesClient.java
@@ -1,5 +1,6 @@
 package org.codeberg.zenxarch.zombies.client;
 
+import java.util.List;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.ArmorRenderer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
@@ -11,28 +12,29 @@ public class ZombiesClient implements ClientModInitializer {
   @Override
   public void onInitializeClient() {
     EntityRendererRegistry.register(EntityType.ZOMBIE, ExtendedZombieEntityRenderer::new);
-    ArmorRenderer.register(
-        new DoorShieldRenderer(),
-        Items.IRON_DOOR,
-        Items.OAK_DOOR,
-        Items.SPRUCE_DOOR,
-        Items.BIRCH_DOOR,
-        Items.JUNGLE_DOOR,
-        Items.ACACIA_DOOR,
-        Items.CHERRY_DOOR,
-        Items.DARK_OAK_DOOR,
-        Items.PALE_OAK_DOOR,
-        Items.MANGROVE_DOOR,
-        Items.BAMBOO_DOOR,
-        Items.CRIMSON_DOOR,
-        Items.WARPED_DOOR,
-        Items.COPPER_DOOR,
-        Items.EXPOSED_COPPER_DOOR,
-        Items.WEATHERED_COPPER_DOOR,
-        Items.OXIDIZED_COPPER_DOOR,
-        Items.WAXED_COPPER_DOOR,
-        Items.WAXED_EXPOSED_COPPER_DOOR,
-        Items.WAXED_WEATHERED_COPPER_DOOR,
-        Items.WAXED_OXIDIZED_COPPER_DOOR);
+    var doors =
+        List.of(
+            Items.IRON_DOOR,
+            Items.OAK_DOOR,
+            Items.SPRUCE_DOOR,
+            Items.BIRCH_DOOR,
+            Items.JUNGLE_DOOR,
+            Items.ACACIA_DOOR,
+            Items.CHERRY_DOOR,
+            Items.DARK_OAK_DOOR,
+            Items.PALE_OAK_DOOR,
+            Items.MANGROVE_DOOR,
+            Items.BAMBOO_DOOR,
+            Items.CRIMSON_DOOR,
+            Items.WARPED_DOOR,
+            Items.COPPER_DOOR,
+            Items.EXPOSED_COPPER_DOOR,
+            Items.WEATHERED_COPPER_DOOR,
+            Items.OXIDIZED_COPPER_DOOR,
+            Items.WAXED_COPPER_DOOR,
+            Items.WAXED_EXPOSED_COPPER_DOOR,
+            Items.WAXED_WEATHERED_COPPER_DOOR,
+            Items.WAXED_OXIDIZED_COPPER_DOOR);
+    for (var door : doors) ArmorRenderer.register(new DoorShieldRenderer(), door);
   }
 }
