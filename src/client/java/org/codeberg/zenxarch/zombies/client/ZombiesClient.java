477bb2cc2a6c74ffd486d0f5288cea3065176e02
ZenXArch@fedora <ZenXArch@fedora.mail>
2025-09-23 12:35:53 +0530

    not use deprecated method

--- src/client/java/org/codeberg/zenxarch/zombies/client/ZombiesClient.java
+++ src/client/java/org/codeberg/zenxarch/zombies/client/ZombiesClient.java
@@ -1,13 +1,13 @@
 package org.codeberg.zenxarch.zombies.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.render.entity.EntityRendererFactories;
 import net.minecraft.entity.EntityType;
 
 public class ZombiesClient implements ClientModInitializer {
 
   @Override
   public void onInitializeClient() {
-    EntityRendererRegistry.register(EntityType.ZOMBIE, ExtendedZombieEntityRenderer::new);
+    EntityRendererFactories.register(EntityType.ZOMBIE, ExtendedZombieEntityRenderer::new);
   }
 }
