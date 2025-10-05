67349204a1ee870609eb67132106a4d371278fc6
ZenXArch <zenxarch@noreply.codeberg.org>
2025-10-05 16:30:23 +0530

    properly fix instance modifier crashes

--- mob_variants_api/src/main/java/org/codeberg/zenxarch/mob_variants_api/variant/effect/single/AttributeModifierEffect.java
+++ mob_variants_api/src/main/java/org/codeberg/zenxarch/mob_variants_api/variant/effect/single/AttributeModifierEffect.java
@@ -6,6 +6,7 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import java.util.List;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.world.ServerWorld;
@@ -37,10 +38,13 @@ public record AttributeModifierEffect(
   public void run(ServerWorld world, LivingEntity target) {
     var instance = target.getAttributeInstance(attribute);
     if (instance == null) return;
-    var filteredModifiers =
-        modifiers.stream().filter(modifier -> !instance.hasModifier(modifier.id())).toList();
-    if (persistent) instance.addPersistentModifiers(filteredModifiers);
-    else filteredModifiers.forEach(instance::addTemporaryModifier);
+    modifiers.forEach(modifier -> addModifier(instance, modifier));
+  }
+
+  private void addModifier(EntityAttributeInstance instance, EntityAttributeModifier modifier) {
+    if (instance.hasModifier(modifier.id())) return;
+    if (persistent) instance.addPersistentModifier(modifier);
+    else instance.addTemporaryModifier(modifier);
   }
 
   @Override
