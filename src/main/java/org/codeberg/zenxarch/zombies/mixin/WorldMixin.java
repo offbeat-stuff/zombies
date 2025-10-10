bc98b7487d828d6963b7e9b0df02d819c7a01fbd
ZenXArch <zenxarch@noreply.codeberg.org>
2025-10-10 20:15:01 +0530

    update to snapshot

---
+++ src/main/java/org/codeberg/zenxarch/zombies/mixin/WorldMixin.java
@@ -0,0 +1,13 @@
+package org.codeberg.zenxarch.zombies.mixin;
+
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(World.class)
+public abstract class WorldMixin {
+  @Unique
+  public long method_8510() {
+    return ((World) (Object) this).method_75260();
+  }
+}
