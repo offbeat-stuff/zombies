4821e9f652402bd6746249b06d3d12308386a7dc
ZenXArch <zenxarch@noreply.codeberg.org>
2025-10-10 19:29:30 +0530

    reset zombie kills on death

--- src/main/java/org/codeberg/zenxarch/zombies/Zombies.java
+++ src/main/java/org/codeberg/zenxarch/zombies/Zombies.java
@@ -1,19 +1,20 @@
 package org.codeberg.zenxarch.zombies;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.attachment.v1.AttachmentRegistry;
+import net.fabricmc.fabric.api.attachment.v1.AttachmentType;
 import net.fabricmc.fabric.api.command.v2.ArgumentTypeRegistry;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.entity.event.v1.ServerEntityCombatEvents;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.command.argument.serialize.ConstantArgumentSerializer;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.registry.tag.EntityTypeTags;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.stat.Stats;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.dynamic.Codecs;
 import net.minecraft.util.math.MathHelper;
 import org.codeberg.zenxarch.zombies.loot_table.condition.ZombieLootConditionTypes;
 import org.codeberg.zenxarch.zombies.loot_table.function.ZombieLootFunctionTypes;
@@ -51,24 +52,39 @@ public class Zombies implements ModInitializer {
     }
 
     SpawnerAttachments.initialize();
-    ServerEntityEvents.ENTITY_LOAD.register(
-        (entity, world) -> {
-          if (entity instanceof ServerPlayerEntity player) updatePlayerStats(world, player);
-        });
 
     ServerEntityCombatEvents.AFTER_KILLED_OTHER_ENTITY.register(
         (world, self, dead, source) -> {
-          if (self instanceof ServerPlayerEntity player && dead instanceof ZombieEntity)
+          if (self instanceof ServerPlayerEntity player
+              && dead.getType().isIn(EntityTypeTags.ZOMBIES)) {
+            onKillZombie(player);
             updatePlayerStats(world, player);
+          }
         });
+
+    ServerPlayerEvents.JOIN.register(
+        (player) -> updatePlayerStats(player.getEntityWorld(), player));
+
+    ServerPlayerEvents.AFTER_RESPAWN.register(
+        (oldPlayer, newPlayer, alive) -> updatePlayerStats(newPlayer.getEntityWorld(), newPlayer));
   }
+
+  public static final AttachmentType<Integer> ZOMBIE_KILLS =
+      AttachmentRegistry.create(
+          id("zombie_kills"),
+          builder -> builder.persistent(Codecs.POSITIVE_INT).initializer(() -> 0));
 
   private static void updatePlayerStats(ServerWorld world, ServerPlayerEntity player) {
     var health = player.getHealth();
     var maxHealth = player.getMaxHealth();
-    var kills = player.getStatHandler().getStat(Stats.KILLED.getOrCreateStat(EntityType.ZOMBIE));
+    var kills = player.getAttachedOrCreate(ZOMBIE_KILLS);
     var newHealth = MathHelper.clamp(6f + MathHelper.lerp(kills / 2500f, 0f, 44f), 6f, 50f);
     player.getAttributeInstance(EntityAttributes.MAX_HEALTH).setBaseValue(newHealth);
     player.setHealth(health * newHealth / maxHealth);
+  }
+
+  private static void onKillZombie(ServerPlayerEntity player) {
+    var kills = player.getAttachedOrCreate(ZOMBIE_KILLS);
+    player.setAttached(ZOMBIE_KILLS, kills + 1);
   }
 }
