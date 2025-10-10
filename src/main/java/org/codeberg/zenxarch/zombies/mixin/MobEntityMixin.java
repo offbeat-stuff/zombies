5e6572659bbcdd89d757d3880779c976c0756b41
ZenXArch <zenxarch@noreply.codeberg.org>
2025-10-10 21:11:04 +0530

    make zombie gamerule work independent of vanilla tag

--- src/main/java/org/codeberg/zenxarch/zombies/mixin/MobEntityMixin.java
+++ src/main/java/org/codeberg/zenxarch/zombies/mixin/MobEntityMixin.java
@@ -1,18 +1,27 @@
 package org.codeberg.zenxarch.zombies.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.registry.tag.TagKey;
 import org.codeberg.zenxarch.zombies.entity.ExtendedZombieEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MobEntity.class)
 public abstract class MobEntityMixin {
 
-  @Inject(method = "method_75128", at = @At("HEAD"), cancellable = true)
-  private void zenxarch$modifyBurnInDaylight(CallbackInfo ci) {
-    if (((Object) this) instanceof ExtendedZombieEntity zombie && !zombie.burnsInDaylight())
-      ci.cancel();
+  @WrapOperation(
+      method = "tickMovement",
+      at =
+          @At(
+              value = "INVOKE",
+              target =
+                  "Lnet/minecraft/entity/EntityType;isIn(Lnet/minecraft/registry/tag/TagKey;)Z"))
+  private boolean zenxarch$modifyBurnInDaylight(
+      EntityType<?> type, TagKey<EntityType<?>> tag, Operation<Boolean> op) {
+    if (((Object) this) instanceof ExtendedZombieEntity zombie) return zombie.burnsInDaylight();
+    return op.call(type, tag);
   }
 }
