e5a99f986fc7ee9bdb08e794fbb4975edec9e6de
ZenXArch <zenxarch@noreply.codeberg.org>
2025-10-17 14:09:27 +0530

    too many deaths give temp difficulty lows

--- src/main/java/org/codeberg/zenxarch/zombies/difficulty/DefaultDifficultyCategories.java
+++ src/main/java/org/codeberg/zenxarch/zombies/difficulty/DefaultDifficultyCategories.java
@@ -2,6 +2,7 @@ package org.codeberg.zenxarch.zombies.difficulty;
 
 import org.codeberg.zenxarch.zombies.difficulty.category.KillCategory;
 import org.codeberg.zenxarch.zombies.difficulty.category.PlayerCategory;
+import org.codeberg.zenxarch.zombies.difficulty.category.PlayerDeathsCategory;
 import org.codeberg.zenxarch.zombies.difficulty.category.TimeCategory;
 
 public interface DefaultDifficultyCategories {
@@ -9,10 +10,13 @@ public interface DefaultDifficultyCategories {
     KillCategory.initialize();
     PlayerCategory.initialize();
     TimeCategory.initialize();
+    PlayerDeathsCategory.initialize();
     DifficultyCategory.addDifficultyStage(PlayerCategory.PLAYER_CATEGORY, (a, b) -> a * b);
     DifficultyCategory.addDifficultyStage(
         TimeCategory.DAYS_CATEGORY, (a, b) -> (a * 0.3 + a * b * 0.5 + b * 0.1) / 0.9);
     DifficultyCategory.addDifficultyStage(KillCategory.KILL_CATEGORY, (a, b) -> a * 0.9 + b * 0.1);
     DifficultyCategory.addDifficultyStage(TimeCategory.TIME_CATEGORY, (a, b) -> a * b);
+    DifficultyCategory.addDifficultyStage(
+        PlayerDeathsCategory.PLAYER_DEATHS_CATEGORY, (a, b) -> a * b);
   }
 }
---
+++ src/main/java/org/codeberg/zenxarch/zombies/difficulty/category/PlayerDeathsCategory.java
@@ -0,0 +1,77 @@
+package org.codeberg.zenxarch.zombies.difficulty.category;
+
+import com.mojang.serialization.Codec;
+import it.unimi.dsi.fastutil.longs.LongArrayList;
+import java.util.List;
+import net.fabricmc.fabric.api.attachment.v1.AttachmentRegistry;
+import net.fabricmc.fabric.api.attachment.v1.AttachmentType;
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
+import org.codeberg.zenxarch.zombies.Zombies;
+import org.codeberg.zenxarch.zombies.difficulty.CachedValue;
+import org.codeberg.zenxarch.zombies.difficulty.DifficultyCategory;
+import org.codeberg.zenxarch.zombies.difficulty.entry.CachedPlayerBasedDifficultyEntry;
+
+public interface PlayerDeathsCategory {
+
+  public static final AttachmentType<CachedValue> DEATH =
+      CachedValue.createAttachmentType(Zombies.id("player_deaths/death_multiplier"));
+
+  public static final CachedPlayerBasedDifficultyEntry DEATH_ENTRY =
+      new CachedPlayerBasedDifficultyEntry(DEATH, PlayerDeathsCategory::getDifficulty, 1);
+
+  public static final Identifier PLAYER_DEATHS_CATEGORY = Zombies.id("player_deaths");
+
+  public static void initialize() {
+    DifficultyCategory.addDifficultyEntry(PLAYER_DEATHS_CATEGORY, DEATH_ENTRY);
+    ServerLivingEntityEvents.AFTER_DEATH.register(
+        (entity, source) -> {
+          if (entity instanceof ServerPlayerEntity player) recordPlayerDeath(player, source);
+        });
+  }
+
+  public static final long TIME_WINDOW = 20 * 60 * 15;
+  public static final int MAX_DEATHS = 10;
+
+  public static final AttachmentType<List<Long>> DEATH_TIMESTAMPS =
+      AttachmentRegistry.createPersistent(
+          Zombies.id("player_death_timestamps"), Codec.list(Codec.LONG));
+
+  private static void recordPlayerDeath(ServerPlayerEntity player, DamageSource source) {
+    var time = player.getEntityWorld().getTime();
+    var deaths = player.getAttachedOrCreate(DEATH_TIMESTAMPS, () -> new LongArrayList(MAX_DEATHS));
+    deaths.add(time);
+    trimDeaths(deaths, time);
+  }
+
+  private static int getAndUpdateDeathCount(ServerPlayerEntity player) {
+    return trimDeaths(
+        player.getAttachedOrCreate(DEATH_TIMESTAMPS), player.getEntityWorld().getTime());
+  }
+
+  private static int trimDeaths(List<Long> deaths, long time) {
+    var deathCount = countRecentDeaths(deaths, time);
+    while (deaths.size() > deathCount) deaths.removeFirst();
+    return deathCount;
+  }
+
+  private static int countRecentDeaths(List<Long> deaths, long time) {
+    for (int i = 1; i <= deaths.size(); i++) {
+      if (i >= MAX_DEATHS) return i;
+      if ((time - deaths.get(deaths.size() - i)) > TIME_WINDOW) return i;
+    }
+    return deaths.size();
+  }
+
+  private static double mapDeathsToDifficulty(int deathCount) {
+    return MathHelper.clampedMap(deathCount, 3, MAX_DEATHS, 1.0, 0.25);
+  }
+
+  private static double getDifficulty(ServerWorld world, ServerPlayerEntity player) {
+    return mapDeathsToDifficulty(getAndUpdateDeathCount(player));
+  }
+}
